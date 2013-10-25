4c4426d812fca357d5187ecd6a57bc4d4dfdf9a9
Shane Reed <shane@cinegrain.com>
2013-10-24 20:36:37 -0700

    catchframe project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catchframe::Application.config.secret_key_base = 'dceebad9225e7869d1d33630182567a894993ea11ba989500229c7bac52fd5b934053e69435ebcb703954ae6e7d62ca8259a1cb8e5426cffef920e92d41b1da0'
